16b0000fdf30fe5fcab021354d08bd06881c97f1
Alex <alexmengue1990@hotmail.com>
2024-04-08 00:52:06 -0300

    att

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,9 @@
 const pianoKeys = document.querySelectorAll('.piano-keys .key');
 
+const playTune = (key) => {
+  //soon
+};
+
 pianoKeys.forEach(key => {
   key.addEventListener('click', () => playTune(key.dataset.key));
 });
