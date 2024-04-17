3efffa328691916a860d94d65437301bf1c908f3
Alex <alexmengue1990@hotmail.com>
2024-04-16 23:47:41 -0300

    att

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
-const pianoKeys = document.querySelectorAll('.piano-keys .key');
+const pianoKeys = document.querySelectorAll('.piano-keys .key'),
+volumeSlider = document.querySelector('.volume-slider input');
 
 let allKeys = [], audio = new Audio('assets/tunes/a.wav');
 
